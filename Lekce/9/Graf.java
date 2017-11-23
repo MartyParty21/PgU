4f032767e175eab22041196194ea8f68af7a4b9c
MartyParty21 <martin.rokyta@seznam.cz>
2017-11-23 16:06:40 +0100

    Pridani Ulohy Graf

---
+++ Lekce/9/Graf.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Graf
+{
+	public static void main(String args[])
+	{
+		System.out.println("Zadejte max. sto kladnych cisel.");
+		int pole[] = new int[100];
+		int maximum = 0;
+		int pocet = 0;
+
+		Scanner sc = new Scanner(System.in);
+
+		for(; pocet < 100; pocet++)
+		{
+			if(sc.hasNextInt())
+			{
+				pole[pocet] = sc.nextInt();
+
+				if(pole[pocet] > maximum)
+				{
+					maximum = pole[pocet];
+				}
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		for(int i = 0; i < pocet; i++)
+		{
+			int pocetKrizku = pole[i];
+			if(maximum > 60)
+			{
+				pocetKrizku = (int)(pole[i] * 60 / maximum + 0.5);
+			}
+			for(int j = 0; j < pocetKrizku; j++)
+			{
+				System.out.print("#");
+			}
+			System.out.println();
+		}
+	}
+}
